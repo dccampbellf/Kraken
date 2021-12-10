575dffe84214d1b59a68a2a5bf429dd876f06ba8
Douglas Clayton Campbell <75812260+dccampbellf@users.noreply.github.com>
2021-12-10 09:27:05 -0500

    Update Entrega_Douglas_C.py

--- Entrega_Douglas_C.py
+++ Entrega_Douglas_C.py
@@ -5,8 +5,7 @@ import urllib.parse
 import hmac
 import base64
 from json import JSONDecodeError
-from pprint import pprint
-import KrakenAPI
+import pykrakenapi
 import krakenex
 import plotly.express as px
 import plotly.graph_objects as go
